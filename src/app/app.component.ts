cc52ef8003b8de241d7c4d3830f4387fd1bf1f44
Eros <eros.montin@gmail.com>
2025-02-26 13:24:03 -0500

    seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { faLinkedin,faFacebook,faInstagram,faGit,faGoogle } from '@fortawesome/free-brands-svg-icons';
-import {faAddressCard} from '@fortawesome/free-regular-svg-icons'
-import {faAt} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faFacebook, faInstagram, faGit, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -16,4 +17,15 @@ export class AppComponent {
   gi = faGit;
   ad = faAddressCard;
   at = faGoogle;
+
+  constructor(private titleService: Title, private meta: Meta) {
+    // Set the browser tab title
+    this.titleService.setTitle('Dr. Eros Montin Ph.D. | Biomedical Research Expert');
+
+    // Update the meta tag with a description
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Explore Dr. Eros Montin expertise in biomedical imaging, big data, clud computing, AI, and biodimensional innovations.'
+    });
+  }
 }
